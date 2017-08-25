0aedc5d80d4dabccd4bd20cea62c1de231fcf82e
MGF15 <mohamed5548@live.com>
2017-08-25 19:04:55 +0300

    v0.2
    some change and speed up

--- P-Decode.py
+++ P-Decode.py
@@ -1,17 +1,20 @@
 import itertools,sys,time
 from hashlib import sha1
+from binascii import unhexlify , hexlify
 
 '''
-android pattern cracker 
+
+android pattern cracker v0.2
 
 Coded by MGF15
 
 file in android ->  /data/system/gesture.key
+
 '''
 
 pattern = "012345678"
 
-p_ = '''
+patt = '''
  +---+---+---+
  | 0 | 1 | 2 |
  +---+---+---+
@@ -23,20 +26,29 @@ p_ = '''
 now=time.time()
 
 def crack(hash):
-	
+	print '''
+	|~)  |~\ _ _ _  _| _
+	|~ ~~|_/}_(_(_)(_|}_ v0.2\n
+		[ {41}ndr0id Pa77ern Cr4ck t00l. ]
+	'''
+	print '[*] Pattern Hash   : %s\n' % hash
+
 	for i in range(4,10):
 
 		for p in itertools.permutations(pattern, i):
 			# pattern -> 1234 -> 01020304 -> \x01\x02\x03\x04 then sha1 hash
-			x = ''.join(['0' + i for i in p]).decode('hex')
-
+			
+			x = unhexlify(''.join(p).replace("","0")[:-1]) # ''.join(['0' + i for i in p])) #.decode('hex')
+			
 			sha1_hash = sha1(x).hexdigest()
-
+			
 			if hash == sha1_hash:
-
-				print '[+] pattern -> ' , ''.join(p) 
-				print p_
-				print '[*] Time : ', round(time.time() - now, 2) , ' seconds '
+				print '[+] Pattern Length : %s\n' % len(p)
+				print '[+] Pattern \t   : %s' % ''.join(p) 
+				print patt
+				print '[*] Time : %s sec' % round(time.time() - now, 2)  
+				exit()
+	print "[-] Pattern not found !"
 if __name__ == "__main__":
 
 	if len(sys.argv) < 3:
@@ -44,7 +56,7 @@ if __name__ == "__main__":
 
 	elif sys.argv[1] == '-f':
 		file = open(sys.argv[2],'rb').read()
-		hash = file.encode('hex')
+		hash = hexlify(file) #file.encode('hex')
 		crack(hash)
 
 	elif sys.argv[1] == '-p':
